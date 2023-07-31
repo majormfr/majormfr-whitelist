e67a018b01b848e1195a9e8952951ba6cf9111b7
majormfr <mohammadfahad.work@gmail.com>
2023-07-31 18:41:35 +0500

    Gates package security gate keeper

--- src/mfr-whitelist.php
+++ src/mfr-whitelist.php
@@ -1,1 +1,8 @@
 <?php
+namespace Majormfr\MajormfrWhitelist;
+
+use Majormfr\MajormfrWhitelist\Includes\Gates\Gates;
+
+$santizePlugin = new Gates();
+$whitelistPlugins = $santizePlugin->fetch_whitelisted_plugins()['whitelist_plugins'] ?? [];
+$installed_plugins = $santizePlugin->fetch_plugins_bpd_from_db();
